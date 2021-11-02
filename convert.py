bb5130893aa6e07825098218af7dbcd5c4f15fa4
Ubuntu <ubuntu@ip-172-31-21-75.us-east-2.compute.internal>
2021-11-02 23:17:41 +0000

    Replace yaml.load with yaml.safe_load

--- convert.py
+++ convert.py
@@ -129,7 +129,7 @@ if __name__=='__main__':
                         # help = "mel or world features.")
 
     args = parser.parse_args()
-    config = yaml.load(open('./config.yaml', 'r'))
+    config = yaml.safe_load(open('./config.yaml', 'r'))
 
     # checkpoint_dir = '../checkpoints/' + args.model + '/' + args.iteration + '.ckpt'
     checkpoint_dir = args.checkpoint
