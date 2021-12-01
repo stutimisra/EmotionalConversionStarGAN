d141bed9671b6a6e42d269ec9d28267534b4bd5e
eric-zhizu <zhou.eric.y@gmail.com>
2021-11-30 20:45:27 -0500

    Draft t-SNE dimensionality reduction visualization

--- plot_emo_embeddings.py
+++ plot_emo_embeddings.py
@@ -28,6 +28,7 @@ Setup (install speechbrain):
     - cd speechbrain
     - pip install -r requirements.txt
     - pip install --editable .
+    - pip install transformers
 
 Args:
     --hf_model_id <Hugging Face Model ID>
@@ -41,6 +42,12 @@ Usage:
 
 import argparse
 import os
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.mainfold import TSNE
+import time
 import torch
 import torch.utils.data as data_utils
 import yaml
@@ -89,9 +96,61 @@ def main(hf_model_id: str) -> None:
     ###
     #   Loop through audio files
     ###
+    data_batches = []
     for _, (wav_forms, wav_lengths), emotion_labels in data_loader:
+        # encoded_batch: (batch_size, 768)
         encoded_batch = classifier.encode_batch(wav_forms, wav_lengths)
-        print("Encoded batch", encoded_batch.shape)
+
+        # encoded_batch_with_labels: (batch_size, 769)
+        encoded_batch_with_labels = torch.cat(
+            (encoded_batch, torch.unsqueeze(emotion_labels, dim=1)),
+            dim=1
+        )
+
+        data_batches.append(encoded_batch_with_labels)
+    data = torch.vstack(data_batches)
+
+    ###
+    #   Put data into a Pandas dataframe for ease of computation and plotting
+    #   Credits: https://towardsdatascience.com/visualising-high-dimensional-datasets-using-pca-and-t-sne-in-python-8ef87e7915b
+    ###
+    n_features = data.shape[1] - 1
+    feature_cols = ['feature' + str(i) for i in range(n_features)]
+    df = pd.DataFrame(data, columns=feature_cols + ['y'])
+
+    # See pretrained_models/CustomEncoder.../label_encoder.ckpy for index => emotion mappings
+    df['label'] = df['y'].apply(lambda index: classifier.hparams.label_encoder.decode_torch(index))
+
+    ###
+    #   Perform dimensionality reduction using t-SNE
+    ###
+    np.random.seed(42)
+    rndperm = np.random.permutation(df.shape[0])
+
+    # Plot only a random 5000 samples to reduce computation time
+    N = 5000
+    df_subset = df.loc[rndperm[:N], :].copy()
+    data_subset = df_subset[feature_cols].values
+
+    # Perform t-SNE
+    time_start = time.time()
+    tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
+    tsne_results = tsne.fit_transform(data_subset)
+    print(f't-SNE done! Time elapsed: {time.time() - time_start} seconds')
+
+    # Add t-SNE results to the data frame to plot
+    df_subset['tsne-2d-one'] = tsne_results[:, 0]
+    df_subset['tsne-2d-two'] = tsne_results[:, 1]
+
+    plt.figure(figsize=(16, 10))
+    sns.scatterplot(
+        x='tsne-2d-one', y='tsne-2d-two',
+        hue='label',
+        palette=sns.color_palette('hls', 4),
+        data=df_subset,
+        legend='full',
+        alpha=0.3
+    )
 
 
 if __name__ == '__main__':
